b98a49da0c226ff73c8c5dddbb3b8d4fce1aecb1
emilierobichaud1 <emilie.robichaud@mail.utoronto.ca>
2020-08-07 19:19:34 -0400

    added image buttons, let buttons show as clickable

--- app/src/main/java/com/pixl/dnddiceroller/MainActivity.java
+++ app/src/main/java/com/pixl/dnddiceroller/MainActivity.java
@@ -3,15 +3,12 @@ package com.pixl.dnddiceroller;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.pixl.dnddiceroller.R;
-
 import android.graphics.PorterDuff;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -22,32 +19,99 @@ import java.util.concurrent.ThreadLocalRandom;
 public class MainActivity extends AppCompatActivity implements ShakeDetector.Listener {
 
     ImageButton fourSidedDie;
+    ImageButton sixSidedDie;
+    ImageButton eightSidedDie;
+    ImageButton tenSidedDie;
+    ImageButton twelveSidedDie;
+    ImageButton twentySidedDie;
     TextView numberText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //initialize shake detector
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         ShakeDetector sd = new ShakeDetector(this);
         sd.start(sensorManager);
 
+        //set screen to activity_main.xml
         setContentView(R.layout.activity_main);
 
+        //initialize image buttons
         fourSidedDie = findViewById(R.id.fourSidedDie);
+        sixSidedDie = findViewById(R.id.sixSidedDie);
+        eightSidedDie = findViewById(R.id.eightSidedDie);
+        tenSidedDie = findViewById(R.id.tenSidedDie);
+        twelveSidedDie = findViewById(R.id.twelveSidedDie);
+        twentySidedDie = findViewById(R.id.twentySidedDie);
+
+        //initialize number text
         numberText = findViewById(R.id.numberText);
 
-        //set die to unselected by default
+        //set all dice to unselected by default
         fourSidedDie.setSelected(false);
+        sixSidedDie.setSelected(false);
+        eightSidedDie.setSelected(false);
+        tenSidedDie.setSelected(false);
+        twelveSidedDie.setSelected(false);
+        twentySidedDie.setSelected(false);
 
-
-
-
+        //set on click listeners for each image button
         fourSidedDie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fourSidedDie.setColorFilter(0x80FFFFFF, PorterDuff.Mode.MULTIPLY);
+                fourSidedDie.setColorFilter(0xCCFFFFFF);
                 fourSidedDie.setSelected(true);
+
+                sixSidedDie.setSelected(false);
+                sixSidedDie.setColorFilter(null);
+                eightSidedDie.setSelected(false);
+                eightSidedDie.setColorFilter(null);
+                tenSidedDie.setSelected(false);
+                tenSidedDie.setColorFilter(null);
+                twelveSidedDie.setSelected(false);
+                twelveSidedDie.setColorFilter(null);
+                twentySidedDie.setSelected(false);
+                twentySidedDie.setColorFilter(null);
+            }
+        });
+
+        sixSidedDie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sixSidedDie.setColorFilter(0xCCFFFFFF);
+                sixSidedDie.setSelected(true);
+
+                fourSidedDie.setSelected(false);
+                fourSidedDie.setColorFilter(null);
+                eightSidedDie.setSelected(false);
+                eightSidedDie.setColorFilter(null);
+                tenSidedDie.setSelected(false);
+                tenSidedDie.setColorFilter(null);
+                twelveSidedDie.setSelected(false);
+                twelveSidedDie.setColorFilter(null);
+                twentySidedDie.setSelected(false);
+                twentySidedDie.setColorFilter(null);
+            }
+        });
+
+        eightSidedDie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eightSidedDie.setColorFilter(0xCCFFFFFF);
+                eightSidedDie.setSelected(true);
+
+                fourSidedDie.setSelected(false);
+                fourSidedDie.setColorFilter(null);
+                sixSidedDie.setSelected(false);
+                sixSidedDie.setColorFilter(null);
+                tenSidedDie.setSelected(false);
+                tenSidedDie.setColorFilter(null);
+                twelveSidedDie.setSelected(false);
+                twelveSidedDie.setColorFilter(null);
+                twentySidedDie.setSelected(false);
+                twentySidedDie.setColorFilter(null);
             }
         });
 
@@ -60,11 +124,21 @@ public class MainActivity extends AppCompatActivity implements ShakeDetector.Lis
             MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.dice);
             mp.start();
         }
-/*        if(sixSidedDie.isSelected()) {
+        if(sixSidedDie.isSelected()) {
             numberText.setText(String.valueOf(String.valueOf(dieRoll(6))));
             MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.dice);
-        mp.start();
-        }*/
+            mp.start();
+        }
+        if(eightSidedDie.isSelected()) {
+            numberText.setText(String.valueOf(String.valueOf(dieRoll(8))));
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.dice);
+            mp.start();
+        }
+        if(tenSidedDie.isSelected()) {
+            numberText.setText(String.valueOf(String.valueOf(dieRoll(10))));
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.dice);
+            mp.start();
+        }
     }
 
     //this function simulates the dice being rolled
